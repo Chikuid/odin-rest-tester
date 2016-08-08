23e93cccc5cfe3e39077b7f9b66946fae59cdbf4
Chikuid <Chikuid119@gmail.com>
2016-08-08 16:05:01 +0530

    Add 4 GET request of RESTful

--- app/scripts/rest_requestor.rb
+++ app/scripts/rest_requestor.rb
@@ -2,5 +2,25 @@ require 'rest-client'
 
 puts "Input the url in format of string default url is http://localhost:3000/users"
 url = gets.chomp
-url = "http://localhost:3000/users" if url.empty?
-puts RestClient.get(url)
+
+flag =1 if url.empty?
+if flag
+	puts "==============================INDEX==============================="
+	puts RestClient.get("http://localhost:3000/users")
+end
+
+if flag
+	puts "==============================EDIT==============================="
+	puts RestClient.get("http://localhost:3000/users/1/edit")
+end
+
+
+if flag
+	puts "==============================SHOW==============================="
+	puts RestClient.get("http://localhost:3000/users/1")
+end
+
+if flag
+	puts "==============================NEW==============================="
+	puts RestClient.get("http://localhost:3000/users/new")
+end
